62e7f23ab0cfe0830275db16d26d7c87d42030e6
Lau Siaw Young <lausiawyoung@gmail.com>
2014-04-18 02:33:12 +0800

    First commit!

--- README.md
+++ README.md
@@ -2,3 +2,36 @@ Dropbox-Folder-Size
 ===================
 
 A small Python script that generates a .txt file of the sizes of your Dropbox folders. Perfect for checking the sizes of folders that you have selectively unsync-ed.
+
+# Script Requirements
+===================
+
+1. Python SDK
+Installation instructions [here](https://www.dropbox.com/developers/core/sdks/python).
+
+2. Dropbox Access Token
+Still new to Dropbox development, so I'm not sure what the proper way of getting an access token is, but the way I did it was by: 
+
+1. Visiting [Dropbox's Developer site and going to App Console](https://www.dropbox.com/developers/apps).
+2. Create a Dropbox API app with "Files and Datastores", "No", "All file types" options.
+3. Get the app key, visit this [website](https://dbxoauth2.site44.com/) and it will generate an access token for you.
+
+# Usage Instructions
+
+Input your access token into the script.
+
+The script accepts 2 variables:
+
+1. Denominator (non-negative integer). 0 for bytes, 1 for KB, 2 for MB, 3 for GB, etc.
+
+2. Levels (non-negative integer). Tells the script how far down the directory tree you want to look. E.g. if set to 1, the script will output the sizes of all your folders in the main Dropbox directory.
+
+# Known Bugs
+
+1. Dropbox still thinks .pages documents are folders. I might or might not get around to fixing this.
+
+# Possible Features
+
+1. Make this script a full-fledged app, with browser redirection for OAuth authentication and stuff.
+2. Add support to run script in sub-folders.
+3. Add a switch to un-ignore files that appear above the set level.
---
+++ dropbox_folder_size.py
@@ -0,0 +1,60 @@
+import dropbox
+
+client = dropbox.client.DropboxClient('<INSERT ACCESS TOKEN>')
+
+# choose 0 to express values in bytes, 1 in KB, 2 in MB, 3 in GB.
+denominator = 2
+
+# choose how many levels you want to segregate by
+# e.g. if levels is 1, will return folder sizes of folders in Dropbox root
+# If you just want to know overall usage of account (i.e. levels = 0), we will run a separate query for it
+levels = 2
+
+if (levels == 0):
+    quota_info = client.account_info()['quota_info']
+    usage = quota_info['shared'] / (1024.0 ** denominator)
+    quota = quota_info['quota'] / (1024.0 ** denominator)
+
+    output.write('You are currently using ' + str(usage) + ' out of ' + str(quota) + ' of space on Dropbox.')
+    output.close()
+    quit()
+
+sizes = {}
+foldersizes = {}
+cursor = None
+
+while cursor is None or result['has_more']:
+    result = client.delta(cursor)
+    for path, metadata in result['entries']:
+        sizes[path] = metadata['bytes'] if metadata else 0
+    cursor = result['cursor']
+
+for path, size in sizes.items():
+
+    segments = path.split('/')
+
+    if (len(segments) > levels + 1):
+        folder = '/'.join(segments[0:levels+1])
+    else:
+        folder = '/'.join(segments[0:len(segments)-1])
+    try:
+        foldersizes[folder] += size
+    except KeyError:
+        foldersizes[folder] = size
+
+if (denominator != 0):
+    for path,size in foldersizes.items():
+        if size:
+            foldersizes[path] = size / (1024.0 ** denominator)
+        else:
+            del foldersizes[path]
+
+output = open('dropbox_folder_sizes.txt', 'w')
+
+output.write('Below is a list of your largest Dropbox folders, ordered from largest to smallest. You chose a drill level of 2. Sizes are expressed as "%d", where 0 is in bytes, 1 is in KB, 2 is in MB, and 3 is in GB. \n' % denominator)
+
+for folder in reversed(sorted(foldersizes.keys(), key=lambda x: foldersizes[x])):
+    output.write('%s: %f' % (folder, foldersizes[folder]) + '\n')
+
+output.close()
+quit()
